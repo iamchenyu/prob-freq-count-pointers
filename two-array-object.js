3a1b23dfca103724ad03e9d7760685cf23f76254
Chenyu Wang <jadeyw7@gmail.com>
2023-06-15 15:29:04 -0400

    twoArrayObject

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,16 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(arr1, arr2) {
+  let result = {};
+
+  if (arr1.length === 0) return result;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (i > arr2.length - 1) {
+      result[arr1[i]] = null;
+    } else {
+      result[arr1[i]] = arr2[i];
+    }
+  }
+
+  return result;
 }
