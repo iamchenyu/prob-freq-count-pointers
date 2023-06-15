7bc12ebcd5b4b14148da677a11582b07c322638d
Chenyu Wang <jadeyw7@gmail.com>
2023-06-15 14:49:21 -0400

    averagePair

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,21 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr, target) {
+  if (arr.length < 2) return false;
+
+  let left = 0,
+    right = arr.length - 1;
+
+  while (left < right) {
+    const avg = (arr[left] + arr[right]) / 2;
+
+    if (avg < target) {
+      left++;
+    } else if (avg > target) {
+      right--;
+    } else {
+      return true;
+    }
+  }
+
+  return false;
 }
