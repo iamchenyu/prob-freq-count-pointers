3d45ae4de1f7461a673fd4125d9cbb834505cc68
Chenyu Wang <jadeyw7@gmail.com>
2023-06-15 14:23:20 -0400

    constructNote

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,23 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(note, string) {
+  const frequencyCounter1 = frequencyCounter(note);
+  const frequencyCounter2 = frequencyCounter(string);
+
+  for (const key in frequencyCounter1) {
+    if (
+      !frequencyCounter2[key] ||
+      frequencyCounter2[key] < frequencyCounter1[key]
+    )
+      return false;
+  }
+
+  return true;
+}
+
+function frequencyCounter(str) {
+  const counter = {};
+  for (const char of str) {
+    counter[char] = counter[char] + 1 || 1;
+  }
+  return counter;
 }
