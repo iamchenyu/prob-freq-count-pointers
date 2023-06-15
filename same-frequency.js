921125eca50e664c3d5e6455fa984a14c0a1f51f
Chenyu Wang <jadeyw7@gmail.com>
2023-06-15 16:06:22 -0400

    separatePositive

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,7 @@
 // add whatever parameters you deem necessary
-function sameFrequency() {
+function sameFrequency(num1, num2) {
+  const str1 = new String(num1);
+  const str2 = new String(num2);
+
+  return str1.length === str2.length;
 }
--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,26 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(arr) {
+  if (arr.length < 2) return arr;
+
+  let left = 0,
+    right = 1;
+
+  while (right < arr.length) {
+    if (arr[left] > 0) {
+      left++;
+      right++;
+    } else {
+      if (arr[right] > 0) {
+        let temp = arr[right];
+        arr[right] = arr[left];
+        arr[left] = temp;
+        left++;
+        right++;
+      } else {
+        right++;
+      }
+    }
+  }
+
+  return arr;
 }
