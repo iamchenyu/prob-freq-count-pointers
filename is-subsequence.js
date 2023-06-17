6b2b553aec03e155ed8ff41e167f9ebf1d7d57f1
Chenyu Wang <jadeyw7@gmail.com>
2023-06-17 13:52:56 -0400

    isSubsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,23 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2) {
+  const lowerStr1 = str1.toLowerCase();
+  const lowerStr2 = str2.toLowerCase();
+
+  if (lowerStr1.length === 0) return true;
+
+  if (lowerStr2.length === 0) return lowerStr1.length === 0;
+
+  let p1 = 0,
+    p2 = 0;
+
+  while (p1 < lowerStr1.length && p2 < lowerStr2.length) {
+    if (lowerStr1[p1] === lowerStr2[p2]) {
+      p1++;
+      p2++;
+    } else {
+      p2++;
+    }
+  }
+
+  return p1 === lowerStr1.length;
 }
--- is-subsequence.test.js
+++ is-subsequence.test.js
@@ -1,13 +1,16 @@
-describe("isSubsequence", function() {
-  it("should return true if the first string is a substring of the second", function() {
+describe("isSubsequence", function () {
+  it("should return true if the first string is a substring of the second", function () {
     expect(isSubsequence("hello", "hello world")).toBe(true);
     expect(isSubsequence("sing", "sting")).toBe(true);
     expect(isSubsequence("abc", "abracadabra")).toBe(true);
+    expect(isSubsequence("", "")).toBe(true);
+    expect(isSubsequence("", "abc")).toBe(true);
   });
 
-  it("should return false if the first string is not a substring of the second", function() {
+  it("should return false if the first string is not a substring of the second", function () {
     expect(isSubsequence("abc", "acb")).toBe(false);
     expect(isSubsequence("notasubstring", "banana")).toBe(false);
     expect(isSubsequence("almost", "almossssss")).toBe(false);
+    expect(isSubsequence("abc", "")).toBe(false);
   });
 });
