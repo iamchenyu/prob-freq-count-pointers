6651ebc5299b73bd3aa17044767afdbca6474759
Chenyu Wang <jadeyw7@gmail.com>
2023-06-21 19:26:39 -0400

    pivot

--- longest-fall.js
+++ longest-fall.js
@@ -1,3 +1,44 @@
 // add whatever parameters you deem necessary
-function longestFall() {
+// Solution 2
+function longestFall(arr) {
+  if (arr.length === 0) return 0;
+
+  let longest = 1,
+    count = 1;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i + 1] < arr[i]) {
+      count++;
+      longest = Math.max(longest, count);
+    } else {
+      count = 1;
+    }
+  }
+
+  return longest;
 }
+
+// Solution 1
+// function longestFall(arr) {
+//   if (arr.length === 0 || arr.length === 1) return arr.length;
+
+//   let longest = [],
+//     result = 0,
+//     left = 0,
+//     right = 1;
+
+//   longest.push(arr[left]);
+
+//   while (right < arr.length) {
+//     if (arr[right] < arr[left]) {
+//       longest.push(arr[right]);
+//     } else {
+//       result = Math.max(result, longest.length);
+//       longest = [arr[left]];
+//     }
+//     left++;
+//     right++;
+//   }
+
+//   return Math.max(result, longest.length);
+// }
--- pivot-index.js
+++ pivot-index.js
@@ -1,3 +1,45 @@
 // add whatever parameters you deem necessary
-function pivotIndex() {
+// Solution 2
+function pivotIndex(arr) {
+  if (arr.length === 0) return -1;
+
+  let rightSum = arr.reduce((accum, curr) => accum + curr, 0);
+  let pivot = -1;
+  let sum = Infinity;
+  let leftSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    rightSum -= arr[i];
+    leftSum += arr[i - 1] || 0;
+    if (leftSum === rightSum) {
+      if (leftSum <= sum) {
+        pivot = i;
+        sum = leftSum;
+      }
+    }
+  }
+
+  return pivot;
 }
+
+// Solution 1
+// function pivotIndex(arr) {
+//   if (arr.length === 0) return -1;
+
+//   let result = -1;
+//   let sum = Infinity;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     const leftSum = arr.slice(0, i).reduce((accum, curr) => accum + curr, 0);
+//     const rightSum = arr.slice(i + 1).reduce((accum, curr) => accum + curr, 0);
+
+//     if (leftSum === rightSum) {
+//       if (leftSum <= sum) {
+//         sum = leftSum;
+//         result = i;
+//       }
+//     }
+//   }
+
+//   return result;
+// }
